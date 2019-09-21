c4062ef8aa4be2a6d824b2420e0937368db3534e
Marina <38983422+MarinaShadow@users.noreply.github.com>
2019-09-21 20:56:18 +0300

    Update 9.js

--- lesson-2/9.js
+++ lesson-2/9.js
@@ -1,9 +1,14 @@
 const arr = [1, 2, 3, 4, 5, 6];
 // [6,5,4,3,2,1]
 
+const reversedArr = [];
+
 let i = arr.length - 1;
+let j = 0;
 
 while (arr[i]) {
-    console.log(arr[i]);
+    reversedArr[j] = arr[i];
     i--;
+    j++;
 }
+console.log(reversedArr);
